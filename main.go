f0cd203af5910bfdbf4a3fc84a7d96ccd5675903
Rainer Miranda <rainer.lopes@gmail.com>
2023-03-10 10:08:53 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,24 @@
+/*
+
+Simple HTTP server written in Go for learning purposes
+
+Example of request: http://localhost:8080/world
+
+*/
+
+
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", HelloServer)
+    http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
